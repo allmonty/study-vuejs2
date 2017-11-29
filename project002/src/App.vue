96292abe3d4d9f2b67a1c15a6c58c238a2115ecd
Allan David <allanbrados@gmail.com>
2017-11-29 18:25:48 -0200

    project002: creates Quote counter component

--- project002/src/App.vue
+++ project002/src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container">
-    
+        <app-quote-counter></app-quote-counter>
     </div>
 </template>
 
 <script>
+    import QuoteCounter from "./quotes/QuoteCounter.vue";
+
     export default {
-        
+        components: {
+            appQuoteCounter: QuoteCounter
+        }
     }
 </script>
 
---
+++ project002/src/quotes/QuoteCounter.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>Quotes Added</h2>
+        <div class="progression-bar" id="back">
+            <div class="progression-bar" id="front" :style="{width: calculateProgression}">
+                <span v-if="countedQuotes > 0">{{countedQuotes}} / {{maxQuotes}}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                countedQuotes: 0,
+                maxQuotes: 10,
+            }
+        },
+        computed: {
+            calculateProgression(){
+                return (this.countedQuotes * 100) / this.maxQuotes + "%"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .progression-bar{
+        width: 100%;
+        height: 25px;
+        border-radius: 5px;
+
+        text-align: center;
+        line-height: 25px;
+        font-weight: bold;
+        color: white;
+
+        transition: width 1s;
+    }
+    .progression-bar#back{
+        background-color: lightgray;
+    }
+    .progression-bar#front{
+        background-color: #7272ff;
+    }
+</style>
